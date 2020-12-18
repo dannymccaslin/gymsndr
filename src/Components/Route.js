a820d74d1d02287ba842597918f3faaef6b007e9
mccaslin.danny@gmail.com <1402643+dannymccaslin@users.noreply.github.com>
2020-12-18 17:07:48 -0500

    Working on climbing grades

--- src/Components/Route.js
+++ src/Components/Route.js
@@ -1,23 +1,39 @@
 import React, { useEffect, useState } from 'react';
+import {climbGrades, boulderGrades} from '../DataFiles/climbgrades';
 
 const Route = ({location}) => {
     const routeId = location.state.id;
     const gymName = location.state.gymName.gymName;
     const [route,setRoute] = useState([]);
+    var grade = '';
 
     useEffect(() => {
         fetch(`https://gymsendr.web.app/routes/${routeId}`)
             .then(res => res.json())
             .then(result => {
                 setRoute(result)
-            }) 
+            });
+
+        if (route.grade >= 2000) {
+            for (const bGrade in boulderGrades) {
+                if (bGrade[value] === route.grade) {
+                    grade = bGrade[hueco];
+                }
+            }
+        } else {
+            for (const cGrade in climbGrades) {
+                if (cGrade[value] === route.grade) {
+                    grade = cGrade[yds];
+                }
+            }
+        }
     },[routeId])
     console.log(route);
 
     return (
         <div>
             <p>{route.routeName}</p>
-            <p>{route.grade}</p>
+            <p>{ route.grade >= 20000 ? climbGrades.}</p>
             <label>Route Color: </label><p>{route.color}</p>
             <p>Route set by {route.setter}</p>
             <p>More routes at {gymName}</p>
